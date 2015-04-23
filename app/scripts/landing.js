a299edc4c00139b4683759c4985c1a4b0e3fa4af
Jessica Appeldoorn <jessica.appeldoorn@gmail.com>
2015-04-23 23:06:47 +0000

    Added jQuery animations to landing page

---
+++ app/scripts/landing.js
@@ -0,0 +1,18 @@
+  $(document).ready(function() { 
+    $('.hero-content h3').click(function(){
+      var subText = $(this).text();
+       $(this).text(subText + "!");
+    });
+ 
+   var onHoverAction = function(event) {
+     console.log('Hover action triggered.');
+     $(this).animate({'margin-top': '10px'});
+   };
+ 
+   var offHoverAction = function(event) {
+     console.log('Off-hover action triggered.');
+     $(this).animate({'margin-top': '0px'});
+   };
+ 
+    $('.selling-points .point').hover(onHoverAction, offHoverAction);
+  });
