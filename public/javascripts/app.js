58f216a6bfd9b2e7fcea1b253182ced43a471c6c
Jessica Appeldoorn <jessica.appeldoorn@gmail.com>
2015-04-24 04:35:53 +0000

    Used jQuery to populate the collection view.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -92,6 +92,79 @@
 })();
 require.register("scripts/app", function(exports, require, module) {
 require("./landing");
+  require('./collection');
+
+});
+
+;require.register("scripts/collection", function(exports, require, module) {
+ var buildAlbumThumbnail = function() {
+    var template =
+        '<div class="collection-album-container col-md-2">'
+      + '  <img src="/images/album-placeholder.png"/>'
+      + '  <div class="caption album-collection-info">'
+      + '    <p>'
+      + '      <a class="album-name" href="/album.html"> Album Name </a>'
+      + '      <br/>'
+      + '      <a href="/album.html"> Artist name </a>'
+      + '      <br/>'
+      + '      X songs'
+      + '      <br/>'
+      + '    </p>'
+      + '  </div>'
+      + '</div>';
+ 
+   return $(template);
+ };
+ 
+var updateCollectionView = function() {
+  var $collection = $(".collection-container .row");
+  $collection.empty();
+
+  for (var = 0; i < 33; i++) {
+    var $newThumbnail = buildAlbumThumbnail();
+    $collection.append($newThumbnail);
+  }
+};
+
+
+if (document.URL.match(/\/collection.html/)) {
+  // Wait until the HTML is fully processed.
+  $(document).ready(function() {
+      updateCollectionView();
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
 ;require.register("scripts/landing", function(exports, require, module) {
