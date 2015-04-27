d977d899a52cfae7845165256957485574174a2a
Jessica Appeldoorn <jessica.appeldoorn@gmail.com>
2015-04-27 18:39:39 +0000

    Added an overlay to the collection view

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -213,7 +213,9 @@ require("./landing");
  var buildAlbumThumbnail = function() {
     var template =
         '<div class="collection-album-container col-md-2">'
-      + '  <img src="/images/album-placeholder.png"/>'
+      + '  <div class="collection-album-image-container">'
+      + '    <img src="/images/album-placeholder.png"/>'
+      + '  </div>'
       + '  <div class="caption album-collection-info">'
       + '    <p>'
       + '      <a class="album-name" href="/album.html"> Album Name </a>'
@@ -228,6 +230,23 @@ require("./landing");
  
    return $(template);
  };
+
+  var buildAlbumOverlay = function(albumURL) {
+    var template =
+        '<div class="collection-album-image-overlay">'
+      + '  <div class="collection-overlay-content">'
+      + '    <a class="collection-overlay-button" href="' + albumURL + '">'
+      + '      <i class="fa fa-play"></i>'
+      + '    </a>'
+      + '    &nbsp;'
+      + '    <a class="collection-overlay-button">'
+      + '      <i class="fa fa-plus"></i>'
+      + '    </a>'
+      + '  </div>'
+      + '</div>'
+      ;
+    return $(template);
+  };
  
 var updateCollectionView = function() {
   var $collection = $(".collection-container .row");
@@ -237,6 +256,16 @@ var updateCollectionView = function() {
     var $newThumbnail = buildAlbumThumbnail();
     $collection.append($newThumbnail);
   }
+
+    var onHover = function(event) {
+      $(this).append(buildAlbumOverlay("/album.html"));
+    };
+
+    var offHover = function(event) {
+      $(this).find('.collection-album-image-overlay').remove();
+    };
+
+    $collection.find('.collection-album-image-container').hover(onHover, offHover);
 };
 
 
