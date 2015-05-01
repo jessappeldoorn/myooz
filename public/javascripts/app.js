00f62b1441c81d8e8b1802c87574270e5c0a6e57
Jessica Appeldoorn <jessica.appeldoorn@gmail.com>
2015-05-01 21:15:11 +0000

    Converted landing page to angular

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -304,10 +304,32 @@ $('.myButton').click(changeAlbumView(albumMarconi));
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
-require("./landing");
-require('./collection');
-require('./album');
-require("./profile");
+// require("./landing");
+// require('./collection');
+// require('./album');
+// require("./profile");
+
+ angular.module('BlocJams', []).controller('Landing.controller', ['$scope', function($scope) {
+  $scope.subText = "Turn the music up!";
+
+      $scope.subTextClicked = function() {
+        $scope.subText += '!';
+      };
+
+   $scope.albumURLs = [
+     '/images/album-placeholders/album-1.jpg',
+     '/images/album-placeholders/album-2.jpg',
+     '/images/album-placeholders/album-3.jpg',
+     '/images/album-placeholders/album-4.jpg',
+     '/images/album-placeholders/album-5.jpg',
+     '/images/album-placeholders/album-6.jpg',
+     '/images/album-placeholders/album-7.jpg',
+     '/images/album-placeholders/album-8.jpg',
+     '/images/album-placeholders/album-9.jpg',
+   ];
+
+ }]);
+
 
 
 });
