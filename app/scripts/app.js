be342a3679e9a1b3560b526a6e59c7c387a8adee
Jessica Appeldoorn <jessica.appeldoorn@gmail.com>
2015-05-06 22:56:50 +0000

    Made next and next and  previous buttons on player-bar work.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -118,6 +118,10 @@
  }]);
  
  blocJams.service('SongPlayer', function() {
+      var trackIndex = function(album, song) {
+        return album.songs.indexOf(song);
+      };
+
    return {
      currentSong: null,
      currentAlbum: null,
@@ -128,6 +132,23 @@
      },
      pause: function() {
        this.playing = false;
+     },
+     next: function() {
+       var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+       currentTrackIndex++;
+       if (currentTrackIndex >= this.currentAlbum.songs.length) {
+         currentTrackIndex = 0;
+       }
+       this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+     },
+     previous: function() {
+       var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+       currentTrackIndex--;
+       if (currentTrackIndex < 0) {
+         currentTrackIndex = this.currentAlbum.songs.length - 1;
+       }
+ 
+       this.currentSong = this.currentAlbum.songs[currentTrackIndex];
      },
      setSong: function(album, song) {
        this.currentAlbum = album;
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -424,6 +424,10 @@ $('.myButton').click(changeAlbumView(albumMarconi));
  }]);
  
  blocJams.service('SongPlayer', function() {
+      var trackIndex = function(album, song) {
+        return album.songs.indexOf(song);
+      };
+
    return {
      currentSong: null,
      currentAlbum: null,
@@ -434,6 +438,23 @@ $('.myButton').click(changeAlbumView(albumMarconi));
      },
      pause: function() {
        this.playing = false;
+     },
+     next: function() {
+       var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+       currentTrackIndex++;
+       if (currentTrackIndex >= this.currentAlbum.songs.length) {
+         currentTrackIndex = 0;
+       }
+       this.currentSong = this.currentAlbum.songs[currentTrackIndex];
+     },
+     previous: function() {
+       var currentTrackIndex = trackIndex(this.currentAlbum, this.currentSong);
+       currentTrackIndex--;
+       if (currentTrackIndex < 0) {
+         currentTrackIndex = this.currentAlbum.songs.length - 1;
+       }
+ 
+       this.currentSong = this.currentAlbum.songs[currentTrackIndex];
      },
      setSong: function(album, song) {
        this.currentAlbum = album;
