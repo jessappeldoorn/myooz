f13850c965a596eeea2633dd7d8ca81f2b46613b
Jessica Appeldoorn <jessica.appeldoorn@gmail.com>
2015-05-08 22:52:41 +0000

    Changed slider to update (music location) data on change.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,11 +13,11 @@
    albumArtUrl: '/images/album-placeholder.png',
  
    songs: [
-       { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-       { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-       { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-       { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-       { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
      ]
  };
  
@@ -158,6 +158,13 @@
       var song = this.currentAlbum.songs[currentTrackIndex];
       this.setSong(this.currentAlbum, song);
      },
+       seek: function(time) {
+         // Checks to make sure that a sound file is playing before seeking.
+         if(currentSoundFile) {
+           // Uses a Buzz method to set the time of the song.
+           currentSoundFile.setTime(time);
+         }
+       },
       setSong: function(album, song) {
         if (currentSoundFile) {
           currentSoundFile.stop();
@@ -187,21 +194,50 @@ blocJams.directive('slider', ['$document', function($document){
      return offsetXPercent;
    }
  
+   var numberFromValue = function(value, defaultValue) {
+     if (typeof value === 'number') {
+       return value;
+     }
+ 
+     if(typeof value === 'undefined') {
+       return defaultValue;
+     }
+ 
+     if(typeof value === 'string') {
+       return Number(value);
+     }
+   }
+ 
+
   return {
     templateUrl: '/templates/directives/slider.html',
     replace: true,
     restrict: 'E',
-    scope: {}, // Creates a scope that exists only in this directive.
+    scope: {
+      onChange: '&'
+    },
     link: function(scope, element, attributes) {
       // These value represent the progress into the song/volumn bar, and it's max values.
       // For now, we're supplying arbitrary initial and max values.
       scope.value = 0;
-      scope.max = 200;
+      scope.max = 100;
 
       var $seekBar = $(element);
 
+        console.log(attributes);
+        attributes.$observe('value', function(newValue) {
+          scope.value = numberFromValue(newValue, 0);
+        });
+   
+        attributes.$observe('max', function(newValue) {
+          scope.max = numberFromValue(newValue, 100) || 100;
+        });
+ 
+
       var percentString = function() {
-        var percent = Number(scope.value) / Number(scope.max) * 100;
+          var value = scope.value || 0;
+          var max = scope.max || 100;
+          percent = value / max * 100;
         return percent + "%";
       }
 
@@ -216,6 +252,8 @@ blocJams.directive('slider', ['$document', function($document){
         scope.onClickSlider = function(event) {
          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
          scope.value = percent * scope.max;
+           notifyCallback(scope.value);
+
        }
 
        scope.trackThumb = function() {
@@ -223,6 +261,8 @@ blocJams.directive('slider', ['$document', function($document){
            var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
            scope.$apply(function(){
              scope.value = percent * scope.max;
+               notifyCallback(scope.value);
+
            });
          });
  
@@ -231,6 +271,12 @@ blocJams.directive('slider', ['$document', function($document){
            $document.unbind('mousemove.thumb');
            $document.unbind('mouseup.thumb');
          });
+       };
+
+       var notifyCallback = function(newValue) {
+         if(typeof scope.onChange === 'function') {
+           scope.onChange({value: newValue});
+         }
        };
  
     }
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -319,11 +319,11 @@ $('.myButton').click(changeAlbumView(albumMarconi));
    albumArtUrl: '/images/album-placeholder.png',
  
    songs: [
-       { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-       { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-       { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-       { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-       { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+      { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+      { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+      { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+      { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+      { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
      ]
  };
  
@@ -464,6 +464,13 @@ $('.myButton').click(changeAlbumView(albumMarconi));
       var song = this.currentAlbum.songs[currentTrackIndex];
       this.setSong(this.currentAlbum, song);
      },
+       seek: function(time) {
+         // Checks to make sure that a sound file is playing before seeking.
+         if(currentSoundFile) {
+           // Uses a Buzz method to set the time of the song.
+           currentSoundFile.setTime(time);
+         }
+       },
       setSong: function(album, song) {
         if (currentSoundFile) {
           currentSoundFile.stop();
@@ -493,21 +500,50 @@ blocJams.directive('slider', ['$document', function($document){
      return offsetXPercent;
    }
  
+   var numberFromValue = function(value, defaultValue) {
+     if (typeof value === 'number') {
+       return value;
+     }
+ 
+     if(typeof value === 'undefined') {
+       return defaultValue;
+     }
+ 
+     if(typeof value === 'string') {
+       return Number(value);
+     }
+   }
+ 
+
   return {
     templateUrl: '/templates/directives/slider.html',
     replace: true,
     restrict: 'E',
-    scope: {}, // Creates a scope that exists only in this directive.
+    scope: {
+      onChange: '&'
+    },
     link: function(scope, element, attributes) {
       // These value represent the progress into the song/volumn bar, and it's max values.
       // For now, we're supplying arbitrary initial and max values.
       scope.value = 0;
-      scope.max = 200;
+      scope.max = 100;
 
       var $seekBar = $(element);
 
+        console.log(attributes);
+        attributes.$observe('value', function(newValue) {
+          scope.value = numberFromValue(newValue, 0);
+        });
+   
+        attributes.$observe('max', function(newValue) {
+          scope.max = numberFromValue(newValue, 100) || 100;
+        });
+ 
+
       var percentString = function() {
-        var percent = Number(scope.value) / Number(scope.max) * 100;
+          var value = scope.value || 0;
+          var max = scope.max || 100;
+          percent = value / max * 100;
         return percent + "%";
       }
 
@@ -522,6 +558,8 @@ blocJams.directive('slider', ['$document', function($document){
         scope.onClickSlider = function(event) {
          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
          scope.value = percent * scope.max;
+           notifyCallback(scope.value);
+
        }
 
        scope.trackThumb = function() {
@@ -529,6 +567,8 @@ blocJams.directive('slider', ['$document', function($document){
            var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
            scope.$apply(function(){
              scope.value = percent * scope.max;
+               notifyCallback(scope.value);
+
            });
          });
  
@@ -537,6 +577,12 @@ blocJams.directive('slider', ['$document', function($document){
            $document.unbind('mousemove.thumb');
            $document.unbind('mouseup.thumb');
          });
+       };
+
+       var notifyCallback = function(newValue) {
+         if(typeof scope.onChange === 'function') {
+           scope.onChange({value: newValue});
+         }
        };
  
     }
