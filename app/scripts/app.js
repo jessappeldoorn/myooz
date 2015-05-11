59d2433462d25041df02f213beb823f459bb05b6
Jessica Appeldoorn <jessica.appeldoorn@gmail.com>
2015-05-11 21:09:21 +0000

    Added a time indicator for song progress

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -118,9 +118,17 @@
 
  blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.songPlayer = SongPlayer;
+  
+     SongPlayer.onTimeUpdate(function(event, time){
+       $scope.$apply(function(){
+         $scope.playTime = time;
+       });
+     });
+   
  }]);
  
- blocJams.service('SongPlayer', function() {
+ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
+
       var currentSoundFile = null;
       var trackIndex = function(album, song) {
         return album.songs.indexOf(song);
@@ -165,21 +173,31 @@
            currentSoundFile.setTime(time);
          }
        },
+
+       onTimeUpdate: function(callback) {
+        return $rootScope.$on('sound:timeupdate', callback);
+       },
+
       setSong: function(album, song) {
         if (currentSoundFile) {
           currentSoundFile.stop();
         }
         this.currentAlbum = album;
         this.currentSong = song;
+
         currentSoundFile = new buzz.sound(song.audioUrl, {
           formats: [ "mp3" ],
           preload: true
         });
-     
+
+         currentSoundFile.bind('timeupdate', function(e){
+          $rootScope.$broadcast('sound:timeupdate', this.getTime());
+        });
+  
         this.play();
       }
    };
- });
+ }]);
 
  
 blocJams.directive('slider', ['$document', function($document){
@@ -282,3 +300,34 @@ blocJams.directive('slider', ['$document', function($document){
     }
   };
 }]);
+
+ // Add this right below the slider directive definition.
+ 
+ blocJams.filter('timecode', function(){
+   return function(seconds) {
+     seconds = Number.parseFloat(seconds);
+ 
+     // Returned when no time is provided.
+     if (Number.isNaN(seconds)) {
+       return '-:--';
+     }
+ 
+     // make it a whole number
+     var wholeSeconds = Math.floor(seconds);
+ 
+     var minutes = Math.floor(wholeSeconds / 60);
+ 
+     remainingSeconds = wholeSeconds % 60;
+ 
+     var output = minutes + ':';
+ 
+     // zero pad seconds, so 9 seconds should be :09
+     if (remainingSeconds < 10) {
+       output += '0';
+     }
+ 
+     output += remainingSeconds;
+ 
+     return output;
+   }
+ });
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -424,9 +424,17 @@ $('.myButton').click(changeAlbumView(albumMarconi));
 
  blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.songPlayer = SongPlayer;
+  
+     SongPlayer.onTimeUpdate(function(event, time){
+       $scope.$apply(function(){
+         $scope.playTime = time;
+       });
+     });
+   
  }]);
  
- blocJams.service('SongPlayer', function() {
+ blocJams.service('SongPlayer', ['$rootScope', function($rootScope) {
+
       var currentSoundFile = null;
       var trackIndex = function(album, song) {
         return album.songs.indexOf(song);
@@ -471,21 +479,31 @@ $('.myButton').click(changeAlbumView(albumMarconi));
            currentSoundFile.setTime(time);
          }
        },
+
+       onTimeUpdate: function(callback) {
+        return $rootScope.$on('sound:timeupdate', callback);
+       },
+
       setSong: function(album, song) {
         if (currentSoundFile) {
           currentSoundFile.stop();
         }
         this.currentAlbum = album;
         this.currentSong = song;
+
         currentSoundFile = new buzz.sound(song.audioUrl, {
           formats: [ "mp3" ],
           preload: true
         });
-     
+
+         currentSoundFile.bind('timeupdate', function(e){
+          $rootScope.$broadcast('sound:timeupdate', this.getTime());
+        });
+  
         this.play();
       }
    };
- });
+ }]);
 
  
 blocJams.directive('slider', ['$document', function($document){
@@ -588,6 +606,37 @@ blocJams.directive('slider', ['$document', function($document){
     }
   };
 }]);
+
+ // Add this right below the slider directive definition.
+ 
+ blocJams.filter('timecode', function(){
+   return function(seconds) {
+     seconds = Number.parseFloat(seconds);
+ 
+     // Returned when no time is provided.
+     if (Number.isNaN(seconds)) {
+       return '-:--';
+     }
+ 
+     // make it a whole number
+     var wholeSeconds = Math.floor(seconds);
+ 
+     var minutes = Math.floor(wholeSeconds / 60);
+ 
+     remainingSeconds = wholeSeconds % 60;
+ 
+     var output = minutes + ':';
+ 
+     // zero pad seconds, so 9 seconds should be :09
+     if (remainingSeconds < 10) {
+       output += '0';
+     }
+ 
+     output += remainingSeconds;
+ 
+     return output;
+   }
+ });
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
